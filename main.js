377c64a9da944ad964f96a06bb112e5f08b364fe
MokshSharma95 <82258587+MokshSharma95@users.noreply.github.com>
2022-05-31 13:46:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+lipsX=0;
+lipsY=0;
+
+function preload() {
+clown_nose = loadImage('https://e7.pngegg.com/pngimages/776/375/png-clipart-moustache-moustache.png');
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelLoded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(clown_nose,noseX,noseY,30,30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
